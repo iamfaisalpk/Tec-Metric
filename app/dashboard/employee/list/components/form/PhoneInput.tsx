42268f9cecd88d91c4553023f8a1734b4db82031
iamfaisalpk <iamfaizy123@gmail.com>
2025-11-18 09:33:22 +0530

    completing ui design the employee

--- app/dashboard/employee/list/components/form/PhoneInput.tsx
+++ app/dashboard/employee/list/components/form/PhoneInput.tsx
@@ -1,49 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-// import { Controller, Control } from 'react-hook-form';
-// import PhoneInputLib from 'react-phone-number-input';
-// import type { CountryCode } from 'libphonenumber-js/core';
-// import 'react-phone-number-input/style.css';
-// import { FormData } from '../EmployeeForm';
-
-// interface PhoneInputProps {
-//     name: keyof FormData;
-//     control: Control<FormData>;
-//     optional?: boolean;
-//     defaultCountry?: CountryCode;
-// }
-
-// export const PhoneInput: React.FC<PhoneInputProps> = ({
-//     name,
-//     control,
-//     optional = false,
-//     defaultCountry = 'IN' as CountryCode,
-// }) => (
-//     <Controller
-//         name={name}
-//         control={control}
-//         render={({ field: { onChange, value } }) => (
-//             <div className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary">
-//                 <PhoneInputLib
-//                     international
-//                     defaultCountry={defaultCountry}
-//                     countrySelectProps={{
-//                         searchable: "true",
-//                     }}
-//                     placeholder={optional ? 'Optional phone' : 'Enter phone number'}
-//                     value={value as string | undefined}
-//                     onChange={(val) => onChange(val ?? '')}
-//                     style={{
-//                         '--PhoneInputCountrySelectArrow-color': '#9CA3AF',
-//                     } as React.CSSProperties}
-//                 />
-//             </div>
-//         )}
-//     />
-// );
-
-
 'use client';
 
 import React from 'react';
@@ -82,7 +36,7 @@ export const PhoneInput: React.FC<PhoneInputProps> = ({
             focus-within:ring-2 focus-within:ring-primary
             focus-within:border-primary bg-white
             pl-10
-          "
+            "
                 >
                     <PhoneInputLib
                         international
